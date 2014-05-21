31907e5a284de0e29ec59aa61ab4fed29e32527d
kivio <kivio@kivio.pl>
2014-05-21 13:40:09 +0200

    get templates and statics from tastytools directly

--- tastytools/templatetags/staticlink.py
+++ tastytools/templatetags/staticlink.py
@@ -1,27 +1,31 @@
 from django import template
 from django.conf import settings
-
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
 
 class StaticLinkNode(template.Node):
     def __init__(self, file_type, file_path):
+        print file_type, file_path
         self.file_type = file_type
         if not file_path.endswith(file_type) and file_type != "img":
-            file_path = "%s.%s" % (file_path, file_type)
+            file_path = "%s/%s.%s" % (file_type, file_path, file_type)
+        else:
+            file_path = "%s/%s" % (file_type, file_path)
         self.file_path = file_path
 
     def render(self, context):
+        tag = ''
         if self.file_type == 'js':
-            tag = "<script type='text/javascript' src='%sjs/%s'></script>"
-            tag %= (settings.STATIC_URL, self.file_path)
+            tag = "<script type='text/javascript' src='%s'></script>"
+            tag %= (reverse('tastytools_static_url', args=[self.file_path]),)
         if self.file_type == 'css':
-            tag = "<link rel='stylesheet' type='text/css' href='%scss/%s'>"
-            tag %= (settings.STATIC_URL, self.file_path)
+            tag = "<link rel='stylesheet' type='text/css' href='%s'>"
+            tag %= reverse('tastytools_static_url', kwargs={'path':self.file_path})
         if self.file_type == 'img':
-            tag = "<img src='%simg/%s'/>"
-            tag %= (settings.STATIC_URL, self.file_path)
+            tag = "<img src='%s'/>"
+            tag %= reverse('tastytools_static_url', kwargs={'path':self.file_path})
 
         return tag
 
--- tastytools/urls.py
+++ tastytools/urls.py
@@ -1,7 +1,16 @@
+import os
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 from views import doc, howto
+from django.views.static import serve
+
+def my_serve(request, path, document_root=None, show_indexes=False, *args, **kwargs):
+    path =  "static/" + path
+    document_root = os.path.dirname(os.path.abspath(__file__))
+    return serve(request, path, document_root, show_indexes)
 
 urlpatterns = patterns('',
     (r'^doc', doc),
     (r'^howto', howto),
+    url(r'^static/(?P<path>.*)$', my_serve, name='tastytools_static_url')
 )
